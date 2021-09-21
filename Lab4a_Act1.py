64b65182ce7d76e5b8a35c95a75b41c190d77854
Bryce Elphingstone <73117281+Pseudomoridin@users.noreply.github.com>
2021-09-21 22:55:38 +0000

    Activity 1 done

--- Lab4a_Act1.py
+++ Lab4a_Act1.py
@@ -1,1 +1,35 @@
-raise Exception("File currently not implemented, comment out exec command to test other files.")
+ticket = float(input("Enter Ticket: "))
+lost_ticket = False
+price = 0
+rate = -1
+if (abs(ticket) != ticket):
+  price += 36
+  ticket = abs(ticket)
+if (ticket // 1 != ticket):
+  ticket = int((ticket // 1) + 1)
+
+days = int(ticket // 24)
+ticket = ticket % 24
+
+for x in range(days):
+  price +=  24
+
+# Price per individual hour
+if (ticket > 0):
+  if ticket > 2:
+    price += 4 * 2
+  else:
+    price += 4 * ticket
+  ticket -= 2
+if (ticket > 0):
+  if ticket > 2:
+    price += 3 * 2
+  else:
+    price += 3 * ticket
+  ticket -= 2
+if (ticket > 0):
+  price += ticket * 1
+
+print("${:.2f}".format(price))
+
+# raise Exception("File currently not implemented, comment out exec command to test other files.")
--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 exec(open("./Lab4a_Act1.py").read())
-exec(open("./Lab4a_Act2.py").read())
-exec(open("./Lab4a_Act3.py").read())
+#exec(open("./Lab4a_Act2.py").read())
+#exec(open("./Lab4a_Act3.py").read())
